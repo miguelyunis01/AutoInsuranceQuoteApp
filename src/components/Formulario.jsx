2e2d2d0119a6051a21e5ce08d154679484304673
miguelyunis01 <miguelyunis01@hotmail.com>
2023-01-27 10:34:53 -0500

    Deployment

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,110 @@
+import { Fragment, useState } from "react";
+import { MARCAS, YEARS, PLANES } from "../constants";
+import useCotizador from "../hooks/useCotizador";
+import Error from "./Error";
+
+
+const Formulario = () => {
+
+    const { datos, handleChangeDatos, error, setError, cotizarSeguro } = useCotizador()
+
+    
+
+    const handleSubmit = e =>{
+        e.preventDefault()
+
+        if(Object.values(datos).includes('')) {
+            setError('Todos los campos son oblitarios')
+            return
+        }
+        setError('')
+
+        
+        cotizarSeguro()
+
+    }
+
+   
+
+    return (
+        <>
+        {error && <Error />}
+         <form
+            onSubmit={handleSubmit}
+         >
+            <div className="my-5 ">
+                <label className="block mb-3 font-bold text-gray-400 uppercase">
+                    Brand
+                </label>
+                <select 
+                    name="marca" 
+                    className="w-full p-3 bg-white border border-gray-200"
+                    onChange={e=> handleChangeDatos(e)}
+                    value={datos.marca}
+                >
+                    <option value="">-- Select Brand --</option>
+                    {MARCAS.map(marca => (
+                        <option
+                            key={marca.id}
+                            value={marca.id}
+                        >
+                            {marca.nombre}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className="my-5 ">
+                <label className="block mb-3 font-bold text-gray-400 uppercase">
+                    Year
+                </label>
+                <select 
+                    name="year" 
+                    className="w-full p-3 bg-white border border-gray-200"
+                    onChange={e=> handleChangeDatos(e)}
+                    value={datos.year}
+                >
+                    <option value="">-- Select Year --</option>
+                    {YEARS.map(year => (
+                        <option
+                            key={year}
+                            value={year}
+                        >
+                            {year}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="my-5 ">
+                <label className="block mb-3 font-bold text-gray-400 uppercase">
+                    Select a Plans
+                </label>
+                <div className="flex gap-3 items-center">
+                        {PLANES.map(plan =>(
+                            <Fragment key={plan.id}>
+                                <label>
+                                    {plan.nombre}
+                                </label>
+                                <input 
+                                    type="radio"
+                                    name="plan"
+                                    value={plan.id}
+                                    onChange={e=> handleChangeDatos(e)}
+                                />
+                            </Fragment>
+                        ))}
+                </div>
+            </div>
+
+
+            <input 
+                type="submit" 
+                className="w-full bg-indigo-500 hover:bg-indigo-600 transition-colors text-white cursor-pointer p-3 uppercase font-bold"
+                value="Quote"
+            />
+        </form>   
+        </>
+    );
+};
+
+export default Formulario;
---
+++ src/context/CotizadorProvider.jsx
@@ -0,0 +1,79 @@
+import { useState, createContext } from "react";
+import  {obtenerDiferenciaYear, calcularMarca, calcularPlan, formatearDinero} from '../helpers/index'
+
+const CotizadorContext = createContext()
+
+
+const CotizadorProvider = ({children}) => {
+
+    const [datos, setDatos ] = useState({
+        marca: '',
+        year:'',
+        plan: ''
+    })
+
+    const [ error, setError ] = useState('')
+    const [resultado, setResultado] = useState(0)
+    const [cargando, setCargando] = useState(false)
+
+    const handleChangeDatos =  e => {
+        setDatos({
+            ...datos,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const cotizarSeguro = () => {
+        // Una Base
+        let resultado = 2000
+
+        // Obtener diferencia de años 
+        const diferencia =  obtenerDiferenciaYear(datos.year)
+       
+        //Hay que restar el 3% por cada año 
+        resultado -= ((diferencia + 3) * resultado) / 100
+
+        //Amerinaco 15%
+        //Europeo 30%
+        //Asiatico 5%
+        resultado *= calcularMarca(datos.marca)
+
+        //Basico 20%
+        //Completo 50%
+        resultado *= calcularPlan(datos.plan)
+        
+        //Formatear dinero 
+        resultado = formatearDinero(resultado)
+        
+        setCargando(true)
+
+        setTimeout(() => {
+            setResultado(resultado)
+            setCargando(false)
+        }, 3000);
+
+        
+    }
+
+    return(
+        <CotizadorContext.Provider
+            value={{
+                datos,
+                handleChangeDatos,
+                error,
+                setError,
+                cotizarSeguro,
+                resultado,
+                cargando
+            }}
+        >
+            {children}
+        </CotizadorContext.Provider>
+    )
+}
+
+export {
+    CotizadorProvider
+}
+
+export default CotizadorContext
